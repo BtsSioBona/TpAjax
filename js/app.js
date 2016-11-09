8a6642088d528eba9ad22210d28307068311732d
Sevenn <thomas.s833@gmail.com>
2016-11-09 16:41:50 +0100

    Requette ajax good Manque la mise en forme coté serveur (php)

--- js/app.js
+++ js/app.js
@@ -2,15 +2,27 @@ $(document).ready(function () {
 
     // fonction de contact ajax
     var dropList = document.getElementsByClassName("ajSelect");
-    
-    console.log(dropList);
 
     for (var i = 0; i < dropList.length; i++) {
         dropList[i].addEventListener('change', ajax)
     }
 
     function ajax() {
-        console.log("test");
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                console.log(this.responseText);
+            }
+
+            else if (this.readyState == 4 && this.status !=  200) {
+                console.log("Erreur ajax :" + this.status);
+            }
+        };
+
+        xhttp.open('POST', "ajax.php", true);
+        xhttp.send();
+
     }
 });
 
