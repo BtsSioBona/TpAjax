474c9859dd3881c3f8c09e150d13b485007ad7f0
Sevenn <thomas.s833@gmail.com>
2016-11-09 21:40:34 +0100

    Recupération et création du tableau fonctionnel

--- ajax.php
+++ ajax.php
@@ -1,19 +1,27 @@
 <?php
 
-try {
-    $connect = new PDO('mysql:host=localhost;dbname=tpajax', 'TpAjax', 'TpAjax');
-    $personnel = $connect->prepare("SELECT * FROM personnel");
-    $personnel->execute();
-    $data = $personnel->fetchAll();
 
-    foreach($data as $row) {
-        print_r($row);
+if ($_POST["action"] === "getResult") {
+
+    $ville = (!isset($_POST["ville"]) or empty($_POST["ville"])) ? 'Toulon' : $_POST["ville"];
+    $sexe = (!isset($_POST["sexe"]) or empty($_POST["sexe"])) ? 'M' : $_POST["sexe"];
+    $codeprojet = (!isset($_POST["codeprojet"]) or empty($_POST["codeprojet"])) ? 'PR1' : $_POST["codeprojet"];
+
+    try {
+        $connect = new PDO('mysql:host=localhost;dbname=tpajax', 'TpAjax', 'TpAjax');
+        $personnel = $connect->prepare("SELECT * FROM personnel WHERE VILLE = :ville AND SEXE = :sexe AND CODEPROJET = :codeprojet");
+        $personnel->bindParam(':ville', $ville);
+        $personnel->bindParam(':sexe', $sexe);
+        $personnel->bindParam(':codeprojet', $codeprojet);
+        $personnel->execute();
+        $data = $personnel->fetchAll();
+
+        echo json_encode($data);
+
+        $personnel->closeCursor();
+
+    } catch (PDOException $e) {
+        print "Erreur !: " . $e->getMessage() . "<br/>";
+        die();
     }
-
-    $personnel->closeCursor();
-
-} catch (PDOException $e) {
-    print "Erreur !: " . $e->getMessage() . "<br/>";
-    die();
 }
-
--- js/app.js
+++ js/app.js
@@ -16,8 +16,7 @@ $(document).ready(function () {
 
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                console.log(this.responseText);
-                createThreadHtml();
+                createThreadHtml(JSON.parse(this.responseText));
             }
 
             else if (this.readyState == 4 && this.status != 200) {
@@ -27,7 +26,7 @@ $(document).ready(function () {
 
         xhttp.open('POST', "ajax.php", true);
         xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        xhttp.send("ville=" + ville + "&sexe=" + sexe + "&codeprojet=" + codeprojet);
+        xhttp.send("ville=" + ville + "&sexe=" + sexe + "&codeprojet=" + codeprojet + "&action=getResult");
 
     }
 
@@ -47,7 +46,20 @@ $(document).ready(function () {
             "<th data-field=\"superieur\">SUPERIEUR</th>" +
             "</thead>";
 
-
+        for (var i = 0; i < data.length; i++) {
+            eTable += "<tr>";
+            eTable += "<td>" + data[i].NUMEMP + "</td>";
+            eTable += "<td>" + data[i].NOMEMP + "</td>";
+            eTable += "<td>" + data[i].PRENOMEMP + "</td>";
+            eTable += "<td>" + data[i].CP + "</td>";
+            eTable += "<td>" + data[i].VILLE + "</td>";
+            eTable += "<td>" + data[i].SEXE + "</td>";
+            eTable += "<td>" + data[i].CODEPROJET + "</td>";
+            eTable += "<td>" + data[i].POSTE + "</td>";
+            eTable += "<td>" + data[i].SALAIRE + "</td>";
+            eTable += "<td>" + data[i].SUPERIEUR + "</td>";
+            eTable += "</tr>";
+        }
 
 
         eTable += "</tbody></table>";
